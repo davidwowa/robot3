02659611096ca085bef6ea6528a25b5a9114e724
Wladimir David Zakrevskyy <wd.zakrevskyy@icloud.com>
2017-03-18 00:44:11 +0100

    - update load scripts

--- lib/load_repos.sh
+++ lib/load_repos.sh
@@ -6,8 +6,8 @@ echo "load wiringPi library"
 git clone https://github.com/davidwowa/WiringPi.git
 #echo "install wiringPi"
 # only first time
-cd WiringPi
-git pull origin
-cd WiringPi
-./build
-cd ../..
+#cd WiringPi
+#git pull origin
+#cd WiringPi
+#./build
+#cd ../..
